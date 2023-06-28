a42a66507c7614a677d3881f7af7324873c7bb8a
Sanyu Rajakumar <srajakum@login008.oscar.ccv.brown.edu>
2023-06-28 16:27:20 -0400

    added missing scvae files

---
+++ scvae/models/__init__.py
@@ -0,0 +1,43 @@
+# ======================================================================== #
+#
+# Copyright (c) 2017 - 2020 scVAE authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ======================================================================== #
+
+__all__ = [
+    "VariationalAutoencoder",
+    "GaussianMixtureVariationalAutoencoder"
+]
+
+import importlib
+import os
+
+import tensorflow
+
+from utilities import suppress_stdout
+with suppress_stdout():
+    importlib.import_module("tensorflow.contrib.layers")
+
+from models.variational_autoencoder import (
+    VariationalAutoencoder)  # noqa: E402
+from models.gaussian_mixture_variational_autoencoder import (
+    GaussianMixtureVariationalAutoencoder)  # noqa: E402
+
+import sys
+sys.path.append("/users/srajakum/vae_rnn/Structure_VAE_scRNA_Simulator/Models/scvae")
+
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
+tensorflow.compat.v1.logging.set_verbosity(tensorflow.compat.v1.logging.ERROR)
